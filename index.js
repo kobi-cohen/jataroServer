8ae2d7a8eb2c3040ba231b69bfc5d2549797fe30
kobi <kobicohencom@gmail.com>
2023-11-23 06:57:07 +0200

    9 try

--- index.js
+++ index.js
@@ -7,20 +7,14 @@ const {client} = require('./connection')
 
 app.use(cors())
 app.use(express.json())
-// app.use(express.urlencoded({extended:false}))
-// const func =async ()=>{
-    
-//     const q =await client.query('select * from workers')
-    
-//     console.log(q.rows)
-// }
+
 console.log('Server Started !')
 const insert = async(tableName,{item,phone,time})=>{
     try{
 
-        await client.connect()
-        console.log('connected ')
-        const i = await client.query(`
+        await client.connect(()=>console.log('connected now !'))
+       
+        await client.query(`
         insert into ${tableName}
         (item,phone,date)
         values('${item}','${phone}','${time}')
@@ -50,9 +44,9 @@ app.get('/res',(req,res)=>{
 app.post('/req',(req,res)=>{
     console.log(req.body)
     console.log('request was fired !')
-    const d = new Date().toString()
-console.log(d)
-insert('items',{item:req.body.item,phone:req.body.phone,time:d})
+    const date = new Date().toString()
+
+insert('items',{item:req.body.item,phone:req.body.phone,time:date})
 res.send('great !')
 })
 
