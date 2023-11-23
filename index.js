58cf10d53bc1df8c49fef36296ab2a4e8abc6ba5
kobi <kobicohencom@gmail.com>
2023-11-23 05:25:23 +0200

    server 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const pg = require('pg')
 const cors = require('cors')
 const express = require('express')
 const app = express()
-
+const port = process.env.PORT || 3000;
 const {client} = require('./connection')
 
 app.use(cors())
@@ -44,4 +44,4 @@ res.send('great !')
 })
 
 
-app.listen(3000)
+app.listen(port)
