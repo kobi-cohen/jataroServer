35af50ddece2034fc9ef64b4bb5eb45d9451bcdc
kobi <kobicohencom@gmail.com>
2023-11-24 04:31:31 +0200

    eleventh

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ app.use(express.json())
 console.log('Server Started !')
 
 const insert = async(tableName,{item,phone,time})=>{
+    const client = new pg.Client ({
+        host: 'dpg-ckt6h68168ec73f0548g-a.oregon-postgres.render.com',
+        port: 5432,
+        database: 'luna_075k',
+        user: 'root',
+        password: 'Z2I5gGNsmzhiU24hZTSr3Uokr0hN1Hil',
+        ssl:true,
+       
+      
+      })
     try{
-        const client = new pg.Client ({
-            host: 'dpg-ckt6h68168ec73f0548g-a.oregon-postgres.render.com',
-            port: 5432,
-            database: 'luna_075k',
-            user: 'root',
-            password: 'Z2I5gGNsmzhiU24hZTSr3Uokr0hN1Hil',
-            ssl:true,
-           
-          
-          })
           
         await client.connect(()=>console.log('connected via object  !'))
        
@@ -40,7 +40,7 @@ const insert = async(tableName,{item,phone,time})=>{
     }
     finally{
         
-         client.end(()=>console.log('disconnected via client'))
+        await client.end(()=>console.log('disconnected via client'))
     }
 
 }
